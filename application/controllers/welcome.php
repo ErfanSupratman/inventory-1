1df7f56ab3e3c3817aa312cb7be9cd4bdb9ec22a
Dikdik Tasdik Laksana <dikdik.zahirra@gmail.com>
2015-06-10 14:06:19 +0700

    update install databse

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -8,11 +8,27 @@ class Welcome extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->external->redirect_logged_in();
+        $this->load->dbforge();
     }
 
     public function index() {
-        
+
         $this->load->view('welcome_message');
+    }
+
+    function install() {
+        $file_content = file('DB/inventory.sql');
+        $query = "";
+        foreach ($file_content as $sql_line) {
+            if (trim($sql_line) != "" && strpos($sql_line, "--") === false) {
+                $query .= $sql_line;
+                if (substr(rtrim($query), -1) == ';') {
+                    echo $query;
+                    $result = mysql_query($query)or die(mysql_error());
+                    $query = "";
+                }
+            }
+        }
     }
 
 }
